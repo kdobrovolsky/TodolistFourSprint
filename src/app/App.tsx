d29a7dc648a001c40d4cfb1af69a2e8ba495fe0b
Добровольский <kdobrovolsky02@gmail.com>
2025-10-16 16:13:13 +0300

    add me йquestion

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,37 @@
-import "./App.css"
 import { Header } from "@/common/components/Header/Header"
-import { useAppSelector } from "@/common/hooks"
+import { useAppDispatch, useAppSelector } from "@/common/hooks"
 import { getTheme } from "@/common/theme"
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
 import { selectThemeMode } from "@/app/app-slice.ts"
 import { ErrorSnackbar } from "@/common/components/ErrorSnackbar/ErrorSnackbar.tsx"
 import { Routing } from "@/common/routing/Routing.tsx"
+import { useEffect, useState } from "react"
+import { initializeAppTC } from "@/features/auth/model/auth-slice.ts"
+import { CircularProgress } from "@mui/material"
+import s from "./App.module.css"
 
 export const App = () => {
   const themeMode = useAppSelector(selectThemeMode)
-
+  const dispatch = useAppDispatch()
   const theme = getTheme(themeMode)
+  const [isInitialized, setIsInitialized] = useState(false)
+
+  useEffect(() => {
+    dispatch(initializeAppTC()).finally(() => setIsInitialized(true))
+  }, [])
+
+  if (!isInitialized) {
+    return (
+      <div className={s.circularProgressContainer}>
+        <CircularProgress size={150} thickness={3} />
+      </div>
+    )
+  }
 
   return (
     <ThemeProvider theme={theme}>
-      <div className={"app"}>
+      <div className={s.app}>
         <CssBaseline />
         <Header />
         <Routing />
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
-import { App } from "./app/App"
+import { App } from "./app/App.tsx"
 import { BrowserRouter } from "react-router"
 
 createRoot(document.getElementById("root")!).render(
